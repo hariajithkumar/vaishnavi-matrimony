8f758518f44b0d9d3ef0790ccad2449b1a2f0e14
hariajithkumar <ajithkumar260598@gmail.com>
2024-01-04 19:41:39 +0530

    package and contact

--- src/Domain/Contact.js
+++ src/Domain/Contact.js
@@ -2,12 +2,53 @@ import React from 'react'
 import Header from '../Common/pages/Header'
 import Footer from '../Common/pages/Footer'
 
+// css path 
+import '../Common/assets/css/main.css'
+
+// image path 
+import slider1 from '../Common/assets/image/slider.png'
+
+
 function Contact() {
   return (
     <div>
-        <Header />
-        <h1 className='text-center py-5'>Contact</h1>
-        <Footer />
+      <Header />
+      <div className='container-70 contact-form'>
+        <div className='login-field'>
+          <div className='row m-0 p-3'>
+            <div className='col-lg-6 col-12 p-0 profile-section contact-details'>
+              <h3 className='py-2'>எங்களை தொடர்பு கொள்ள</h3>
+              <hr />
+              <p>செந்தில் குமார் : 99949 02887</p>
+              <p>அலுவலகம் எண் : 99949 02887 , 99949 02887</p>
+              <p>50,பெருமாள் கோயில் சாலை,பாப்பநாயக்கன் பாளையம்,கோவை-641037.</p>
+              <p>கிளைகள் : திருச்சி ,ஈரோடு ,தாராபுரம்</p>
+            </div>
+            <div className='col-lg-6 col-12 mt-lg-0 mt-3 profile-section'>
+              <h3 className='pb-2'>Enquiry Form</h3>
+              <hr />
+              <form>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Your Name</label>
+                  <input type="text" class="form-control" placeholder='Enter your Name' />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email</label>
+                  <input type="text" class="form-control" placeholder='Email' />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Message</label>
+                  <textarea className="form-control" placeholder='Your Message'></textarea>
+                </div>
+              </form>
+              <div>
+                <button className='preview-btn'>Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/Domain/Package.js
+++ src/Domain/Package.js
@@ -2,12 +2,84 @@ import React from 'react'
 import Header from '../Common/pages/Header'
 import Footer from '../Common/pages/Footer'
 
+// image path 
+import slider1 from '../Common/assets/image/slider.png'
+
+// css path 
+import '../Common/assets/css/package.css'
+
+
 function Package() {
   return (
     <div>
-        <Header />
-        <h1 className='text-center py-5'>Packgae</h1>
-        <Footer />
+      <Header />
+      <section class="package-area py-5">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-lg-3 col-md-4 col-sm-6 col-12 col-xs-6 mb-5">
+              <div class="package-rate position-relative">
+                <div className='package-title'>
+                  <h3 class="title text-center">Package</h3>
+                  <p>6 மாதம் செல்லுபடியாகும்</p>
+                  <p></p>
+                </div>
+                <div class="thum text-center">
+                  <img src={slider1} alt="" />
+                </div>
+                <h1 class="text-center m-0 price-title">சாதாரண குழு</h1>
+                <h2 class="price text-center m-0">1000</h2>
+                <a class="text-center book-btn" href="#"><span> Book Package</span></a>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-12 col-xs-6 mb-5">
+              <div class="package-rate position-relative">
+                <div className='package-title'>
+                  <h3 class="title text-center">Package</h3>
+                  <p>6 மாதம் செல்லுபடியாகும்</p>
+                  <p></p>
+                </div>
+                <div class="thum text-center">
+                  <img src={slider1} alt="" />
+                </div>
+                <h1 class="text-center m-0 price-title">விஐபி குழு</h1>
+                <h2 class="price text-center m-0">2000</h2>
+                <a class="text-center book-btn" href="#"><span> Book Package</span></a>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-12 col-xs-6 mb-5">
+              <div class="package-rate position-relative">
+                <div className='package-title'>
+                  <h3 class="title text-center">Package</h3>
+                  <p>6 மாதம் செல்லுபடியாகும்</p>
+                  <p></p>
+                </div>
+                <div class="thum text-center">
+                  <img src={slider1} alt="" />
+                </div>
+                <h1 class="text-center m-0 price-title">விவிஐபி குழு</h1>
+                <h2 class="price text-center m-0">3000</h2>
+                <a class="text-center book-btn" href="#"><span> Book Package</span></a>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-12 col-xs-6 mb-5">
+              <div class="package-rate position-relative">
+                <div className='package-title'>
+                  <h3 class="title text-center">Package</h3>
+                  <p>6 மாதம் செல்லுபடியாகும்</p>
+                  <p></p>
+                </div>
+                <div class="thum text-center">
+                  <img src={slider1} alt="" />
+                </div>
+                <h1 class="text-center m-0 price-title">நட்சத்திரம் குழு</h1>
+                <h2 class="price text-center m-0">5000</h2>
+                <a class="text-center book-btn" href="#"><span> Book Package</span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   )
 }
